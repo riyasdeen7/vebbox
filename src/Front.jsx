ca027314e843598b50d5399e68050af49fddb060
Riyas Deen <riyasdeen@gmail.com>
2026-01-05 12:07:22 +0530

    up

--- src/Front.jsx
+++ src/Front.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 export default function Front() {
   return (
+    <>
     <div>Front baldfg</div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam dicta iste enim quas soluta omnis! Illum deleniti quaerat quam quasi! Quaerat excepturi a debitis saepe. Sed voluptates eligendi sunt dolore.</p>
+    </>
+   
  
 
   
